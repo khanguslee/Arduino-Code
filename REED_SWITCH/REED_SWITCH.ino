63bfd81efb977e5c2a08d626bf8ebb9611ffda77
khanguslee <angus.lee809@gmail.com>
2019-03-21 21:40:30 +1100

    Calculate distance through reed switch

--- REED_SWITCH/REED_SWITCH.ino
+++ REED_SWITCH/REED_SWITCH.ino
@@ -1,5 +1,6 @@
 static const int REED_SWITCH_PIN = 2;
 volatile unsigned long PREV_TIME = 0;
+volatile unsigned long REED_SWITCH_COUNTER = 0;
 void setup() {
   pinMode(REED_SWITCH_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), reedSwitchInterruptHandler, FALLING); // interrrupt 1 is data ready
@@ -7,7 +8,6 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("OFF");
   delay(100);
 }
 
@@ -19,17 +19,21 @@ void reedSwitchInterruptHandler() {
   }
   unsigned long current_time = millis();
   unsigned long total_time = current_time - PREV_TIME;
-  if(total_time == 0) {
+  // 50ms means we have a maximum velocity of 180km/h with a wheel diameter of 0.8
+  if(total_time <= 50) {
     return;
   }
-  
-  float diameter = 0.8;
-  double velocity = (1/total_time) * PI * diameter * 3.6;
-  Serial.println("ON");
-  Serial.println(total_time);
+  REED_SWITCH_COUNTER = REED_SWITCH_COUNTER + 1;
+  // 700C Rims + 33mm tyre
+  const float diameter = 0.622 + (0.033 * 2);
+  float velocity = (1/(float)total_time) * PI * diameter * 3600;
 
-  Serial.print("Velocity: ");
-  Serial.println(velocity);
+  //Serial.print("Velocity: ");
+  Serial.println(String(velocity, 4) + ",");
+
+  float distance = REED_SWITCH_COUNTER * diameter;
+  //Serial.print("Distance: ");
+  //Serial.println(String(distance, 4));
   PREV_TIME = current_time;
 }
 
