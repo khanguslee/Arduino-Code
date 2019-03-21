531314d44fb6dc3e09bffdc8111247e2c872eee8
khanguslee <angus.lee809@gmail.com>
2019-03-22 00:25:01 +1100

    Check if wheel has stopped every 3 seconds

--- REED_SWITCH/REED_SWITCH.ino
+++ REED_SWITCH/REED_SWITCH.ino
@@ -1,10 +1,21 @@
 static const int REED_SWITCH_PIN = 2;
 volatile unsigned long PREV_TIME = 0;
 volatile unsigned long REED_SWITCH_COUNTER = 0;
+volatile unsigned long PREV_REED_SWITCH_COUNT = 0;
+volatile float VELOCITY = 0, DISTANCE = 0;
+
+// 700C Rims + 33mm tyre
+const float WHEEL_DIAMETER = 0.622 + (0.033 * 2);
+  
+IntervalTimer reedSwitchTimer;
+
 void setup() {
   pinMode(REED_SWITCH_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), reedSwitchInterruptHandler, FALLING); // interrrupt 1 is data ready
   Serial.begin(9600);
+
+  // Check if stopped every 3 seconds
+  reedSwitchTimer.begin(reedSwitchChecker, 3 * 1000000);
 }
 
 void loop() {
@@ -15,6 +26,7 @@ void reedSwitchInterruptHandler() {
   // Get start time
   if(PREV_TIME == 0) {
     PREV_TIME = millis();
+    VELOCITY = 0;
     return;
   }
   unsigned long current_time = millis();
@@ -23,17 +35,27 @@ void reedSwitchInterruptHandler() {
   if(total_time <= 50) {
     return;
   }
+
+  // Count how many times wheel rotates
   REED_SWITCH_COUNTER = REED_SWITCH_COUNTER + 1;
-  // 700C Rims + 33mm tyre
-  const float diameter = 0.622 + (0.033 * 2);
-  float velocity = (1/(float)total_time) * PI * diameter * 3600;
 
-  //Serial.print("Velocity: ");
-  Serial.println(String(velocity, 4) + ",");
-
-  float distance = REED_SWITCH_COUNTER * diameter;
-  //Serial.print("Distance: ");
-  //Serial.println(String(distance, 4));
+  // Calculate velocity and distance
+  VELOCITY = (1/(float)total_time) * PI * WHEEL_DIAMETER * 3600;
+  DISTANCE = REED_SWITCH_COUNTER * WHEEL_DIAMETER;
   PREV_TIME = current_time;
+}
+
+// Check if the bike has stopped
+void reedSwitchChecker() {
+  if(PREV_REED_SWITCH_COUNT != REED_SWITCH_COUNTER) {
+    PREV_REED_SWITCH_COUNT = REED_SWITCH_COUNTER;
+  } else {
+    // Stopped!
+    noInterrupts();
+    PREV_TIME = 0;
+    VELOCITY = 0;
+    interrupts();
+  }
+    
 }
 
